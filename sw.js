34d707fc1cb8e6f09f9d0f1da0df4a8f913b5971
vc7017227355-byte <vc7017227355@gmail.com>
2026-06-02 19:14:43 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+// Infinity Heal Service Worker
+const CACHE = 'ih-v1';
+const ASSETS = ['/', '/index.html', '/app.jsx', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
